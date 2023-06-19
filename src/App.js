db70b9294630682ca4015e8cf52dba07b5cc02d7
natthawe <natthawejumjai@gmail.com>
2023-06-19 18:46:21 +0700

    create -Header -Body -Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, React</h1>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
