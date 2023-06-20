d79e0e3e520e0b4ecb1aef7cd9492457440a72c8
natthawe <natthawejumjai@gmail.com>
2023-06-20 18:22:08 +0700

    update

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -2,18 +2,34 @@ import React, { Component } from 'react'
 import { Alert } from 'react-bootstrap'
 
 export default class Connection extends Component {
-  state = { connected: true}
+  state = { connected: true, ros: null }
 
-  init_connection(){
-    var ros = new window.ROSLIB.Ros()
+  // constructor() {
+  //   super()
+  //   this.init_connection()
+  // }
+
+  init_connection() {
+    this.state.ros = new window.ROSLIB.Ros()
+    console.log(this.state.ros)
+
+    // this.state.ros.on("connection", () => {
+    //   console.log("connection established!")
+    //   this.setState({ connected: true })
+    // })
+
+    // this.state.ros.on("close", () => {
+    //   console.log("connection is closed!")
+    //   this.setState({ connected: true })
+    // })
   }
 
   render() {
-    return(
+    return (
       <div>
-        <Alert className='text-center m-5' variant={this.state.connected ? "success" : "danger"}>
-          {this.state.connected ? "Robot Connected" : "Robot Disconnect"}
-        </Alert>    
+        <Alert className='text-center m-6' variant={this.state.connected ? "success" : "danger"}>
+          {this.state.connected ? "Robot Connected" : "Robot Disconnected"}
+        </Alert>
       </div>
     )
   }
