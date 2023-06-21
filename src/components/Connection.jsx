fbc3b8710a8e521c59abdb67c29dbdf7dd509050
natthawe <natthawejumjai@gmail.com>
2023-06-21 18:17:06 +0700

    update

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,62 +1,77 @@
-import React, { Component } from 'react'
-import { Alert } from 'react-bootstrap'
-import ROSLIB from 'roslib'
+import React, { Component } from "react";
+import { Alert } from "react-bootstrap";
+import ROSLIB from "roslib";
 
 export default class Connection extends Component {
-
-  componentDidMount() {
-    this.init_connection()
+  constructor(props) {
+    super(props);
+    this.state = {
+      connectionStatus: null,
+      error: null,
+    };
   }
 
-  init_connection() {
-    const ros = new ROSLIB.Ros()
-    // If there is an error on the backend, an 'error' emit will be emitted.
-    ros.on('error', function(error) {
-      document.getElementById('connecting').style.display = 'none';
-      document.getElementById('connected').style.display = 'none';
-      document.getElementById('closed').style.display = 'none';
-      document.getElementById('error').style.display = 'inline';
+  componentDidMount() {
+    this.initConnection();
+  }
+
+  initConnection() {
+    const ros = new ROSLIB.Ros();
+    ros.on("error", (error) => {
       console.log(error);
+      this.setState({
+        connectionStatus: "error",
+        error: error.message,
+      });
     });
 
-    // Find out exactly when we made a connection.
-    ros.on('connection', function() {
-      console.log('Connected!');
-      document.getElementById('connecting').style.display = 'none';
-      document.getElementById('error').style.display = 'none';
-      document.getElementById('closed').style.display = 'none';
-      document.getElementById('connected').style.display = 'inline';
+    ros.on("connection", () => {
+      console.log("Robot Connected!");
+      this.setState({
+        connectionStatus: "connected",
+        error: null,
+      });
     });
 
-    ros.on('close', function() {
-      console.log('Connection closed!');
-      document.getElementById('connecting').style.display = 'none';
-      document.getElementById('connected').style.display = 'none';
-      document.getElementById('closed').style.display = 'inline';
+    ros.on("close", () => {
+      console.log("Robot Disconnected!");
+      this.setState({
+        connectionStatus: "closed",
+        error: null,
+      });
     });
 
-    // Create a connection to the rosbridge WebSocket server.
-    ros.connect('ws://localhost:9090');
-  }  
-
-
+    ros.connect("ws://localhost:9090");
+  }
 
   render() {
+    const { connectionStatus, error } = this.state;
+
+    let alertVariant;
+    let alertMessage;
+
+    if (connectionStatus === "connected") {
+      alertVariant = "success";
+      alertMessage = "Robot Connected";
+    } else if (connectionStatus === "error" || connectionStatus === "closed") {
+      alertVariant = "danger";
+      alertMessage = connectionStatus === "error" ? "Error" : "Connection Closed";
+    } else {
+      alertVariant = "secondary";
+      alertMessage = "Connecting...";
+    }
+
     return (
       <div>
-        <div id="connecting">
-          Connecting to rosbridge...
-        </div>
-        <div id="connected">
-          Connected
-        </div>
-        <div id="error">
-          Error in the backend!
-        </div>
-        <div id="closed">
-          Connection closed.
-        </div>
+        <Alert className="text-center m-6" variant={alertVariant}>
+          {alertMessage}
+        </Alert>
+        {error && (
+          <Alert className="text-center m-6" variant="danger">
+            Error: {error}
+          </Alert>
+        )}
       </div>
-    )
+    );
   }
 }
